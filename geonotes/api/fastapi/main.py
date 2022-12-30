38c1f170ee4dc91c86b6987b67c9a491466fe033
Mohsen Barzegar <mohsenbarzegar@disroot.org>
2022-12-30 20:38:15 +0330

    add note create endpoint to fastapi api

--- geonotes/api/fastapi/main.py
+++ geonotes/api/fastapi/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 
 from geonotes.domain import note as n
 from geonotes.repository import memrepo as mr
+from geonotes.requests.add_note_request import AddNoteRequest
 from geonotes.requests.note_list_request import NoteListRequest
-from geonotes.usecases import note_list
+from geonotes.usecases import add_note, note_list
 
 from .sample_data import note_dicts
 
@@ -16,4 +17,13 @@ def read_notes() -> list[n.Note]:
     note_list_usecase = note_list.NoteListUseCase(repo)
     request_obj = NoteListRequest(filters={})
     response = note_list_usecase.execute(request_obj)
+    return response.value
+
+
+@app.post("/note", response_model=n.Note)
+def create_note(note: n.Note) -> n.Note:
+    repo = mr.MemRepo(note_dicts)
+    add_note_usecase = add_note.AddNoteUseCase(repo)
+    request_obj = AddNoteRequest(note)
+    response = add_note_usecase.execute(request_obj)
     return response.value
--- geonotes/api/fastapi/tests/test_note.py
+++ geonotes/api/fastapi/tests/test_note.py
@@ -19,10 +19,24 @@ note_list = [note]
 @mock.patch("geonotes.usecases.note_list.NoteListUseCase")
 def test_read_notes(mock_usecase) -> None:
     mock_usecase().execute.return_value = res.ResponseSuccess(note_list)
-    t = res.ResponseSuccess(note_list)
 
     response = client.get("/note")
 
     mock_usecase().execute.assert_called()
     assert response.status_code == 200
     assert response.json() == [note.to_dict()]
+
+
+@mock.patch("geonotes.usecases.add_note.AddNoteUseCase")
+def test_create_note(mock_usecase) -> None:
+    new_note = note.to_dict()
+    new_note.pop("code", None)
+    mock_usecase().execute.return_value = res.ResponseSuccess(new_note)
+
+    response = client.post("/note", json=new_note)
+    result = response.json()
+    result.pop("code", None)
+
+    assert result == new_note
+    assert response.status_code == 200
+    mock_usecase().execute.assert_called()
