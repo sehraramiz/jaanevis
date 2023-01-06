68dac02ce15212fa01742ec9d5d4f9ddb86c9248
Mohsen Barzegar <mohsenbarzegar@disroot.org>
2023-01-06 13:18:42 +0330

    feat(ui): show create form in panel on map click, show details form on note click, clear map markers before showing notes on map, add note delete, update methods

--- jaanevis/ui/app.js
+++ jaanevis/ui/app.js
@@ -7,6 +7,14 @@ createApp({
       title: 'Jaanevis',
       version: '0.0.1',
       baseUrl: 'http://127.0.0.1:8000',
+      panelView: "details",
+      note: {
+        code: "code",
+        creator: "default",
+        url: "",
+        lat: "0.0",
+        long: "0.0"
+      }
     }
   },
   methods: {
@@ -18,6 +26,7 @@ createApp({
           maxZoom: 10,
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(this.map);
+      this.map.on('click', this.showNoteCreateForm);
     },
     onEachFeature: function (feature, layer) {
       var content = `
@@ -25,32 +34,41 @@ createApp({
           <span><b>link</b>: </span>
           <a target="_blank" href="${feature.properties.url}">${feature.properties.url}</a>
       `;
+      layer.on('click', this.showNoteDetailsForm);
       if (feature.properties && feature.properties.creator) {
           layer.bindPopup(content);
       }
     },
+    clearMapNotes: function () {
+      if (this.notesLayer)
+        this.notesLayer.clearLayers();
+    },
     showNotesOnMap: async function () {
-      let url = this.baseUrl + '/note/geojson';
-      const response = await fetch(url)
-      const notes = await response.json()
+      this.clearMapNotes();
+
+      let notes = await this.readNotes();
 
       var notesCollection = {
           'type': 'FeatureCollection',
           'features': notes
-      }
-
-      L.geoJSON(notesCollection, {
+      };
+      this.notesLayer = L.geoJSON(notesCollection, {
           pointToLayer: function (feature, latlng) {
               return L.circleMarker(latlng, {});
           },
-          onEachFeature: this.onEachFeature
+          onEachFeature: this.onEachFeature,
       }).addTo(this.map);
     },
+    readNotes: async function () {
+      let url = this.baseUrl + '/note/geojson';
+      const response = await fetch(url);
+      return await response.json();
+    },
     createNewNote: async function () {
-      var note = {
-        url: this.url,
-        lat: this.lat,
-        long: this.long
+      let note = {
+        url: this.note.url,
+        lat: this.note.lat,
+        long: this.note.long
       };
       const response = await fetch(this.baseUrl + "/note", {
         method: 'POST',
@@ -63,6 +81,56 @@ createApp({
       const content = await response.json();
       this.showNotesOnMap();
       alert("Note created!");
+    },
+    deleteNote: async function () {
+      const response = await fetch(this.baseUrl + `/note/${this.note.code}`, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+        },
+      });
+      const content = await response.json();
+      this.showNotesOnMap();
+      alert("Note deleted!");
+    },
+    updateNote: async function () {
+      let note = {
+        url: this.note.url,
+        lat: this.note.lat,
+        long: this.note.long
+      };
+      const response = await fetch(this.baseUrl + `/note/${this.note.code}`, {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+      });
+      const content = await response.json();
+      this.showNotesOnMap();
+      alert("Note updated!");
+    },
+    showNoteDetailsForm: function (e) {
+      this.panelView = 'details';
+      var properties = e.target.feature.properties;
+      this.note = {
+        code: properties.code,
+        creator: properties.creator,
+        url: properties.url,
+        lat: e.latlng.lat,
+        long: e.latlng.lng,
+      };
+    },
+    showNoteCreateForm: function (e) {
+      this.panelView = 'create';
+      this.note = {
+        code: "",
+        creator: "",
+        url: "",
+        lat: e.latlng.lat,
+        long: e.latlng.lng
+      }
     },
   },
   mounted() {
