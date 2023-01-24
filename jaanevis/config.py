03b02122ec9b0369ea45dfb15dc08f23761ab5a4
Mohsen Barzegar <mohsenbarzegar@disroot.org>
2023-01-24 23:43:07 +0330

    change config cors_origins to accept string of urls

--- jaanevis/config.py
+++ jaanevis/config.py
@@ -1,7 +1,7 @@
 import os
 from typing import Optional
 
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, validator
 
 import jaanevis
 
@@ -9,8 +9,16 @@ import jaanevis
 class Settings(BaseSettings):
     PROJECT_NAME: str
     PROJECT_URL: str
-    CORS_ORIGINS: list[AnyHttpUrl] = []
     BASE_DIR = os.path.dirname(os.path.realpath(jaanevis.__file__))
+    CORS_ORIGINS: list[AnyHttpUrl] | str = []
+
+    @validator("CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: str | list[str]) -> list[str] | str:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
 
     SMTP_TLS: bool = True
     SMTP_PORT: Optional[int] = None
