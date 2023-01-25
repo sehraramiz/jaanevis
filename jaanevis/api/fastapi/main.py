cc63c698a55dec1fc1adce25e58810c913c850b3
Mohsen Barzegar <mohsenbarzegar@disroot.org>
2023-01-25 15:06:13 +0330

    add project base dir to index route

--- jaanevis/api/fastapi/main.py
+++ jaanevis/api/fastapi/main.py
@@ -18,5 +18,5 @@ app.add_middleware(
 
 app.include_router(router, prefix=settings.API_V1_STR)
 app.mount(
-    "/", StaticFiles(directory="jaanevis/ui/dist", html=True), name="static"
+    "/", StaticFiles(directory=settings.BASE_DIR / "ui/dist", html=True), name="static"
 )
